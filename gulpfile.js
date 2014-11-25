224a1ee5190b9fcfe1b46a44acd71a649ffb4e5f
John Martin <me@johnmart.in>
2014-11-25 16:12:27 +0000

    Added clean task

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var concat = require('gulp-concat');
 var less = require('gulp-less');
 var uglify = require('gulp-uglify');
 var compressor = require('gulp-compressor');
+var del = require('del');
 
 var paths = {
   src: {
@@ -43,45 +44,51 @@ var paths = {
   }
 };
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   return gulp.src(paths.src.scripts)
     .pipe(uglify())
     .pipe(gulp.dest(paths.dest.scripts));
 });
 
-gulp.task('less', function() {
+gulp.task('less', function () {
   return gulp.src(paths.src.less)
     .pipe(less())
     .pipe(compressor())
     .pipe(gulp.dest(paths.dest.less));
 });
 
-gulp.task('images', function() {
+gulp.task('images', function () {
   return gulp.src(paths.src.images)
     .pipe(imagemin({ optimizationLevel: 5 }))
     .pipe(gulp.dest(paths.dest.images));
 });
 
-gulp.task('vendor_scripts', function() {
+gulp.task('vendor_scripts', function () {
   return gulp.src(paths.src.vendor_scripts)
     .pipe(uglify())
     .pipe(concat('vendor.js'))
     .pipe(gulp.dest(paths.dest.vendor_scripts));
 });
 
-gulp.task('vendor_styles', function() {
+gulp.task('vendor_styles', function () {
   return gulp.src(paths.src.vendor_styles)
     .pipe(concat('vendor.css'))
     .pipe(gulp.dest(paths.dest.vendor_styles));
 });
 
-gulp.task('vendor_fonts', function() {
+gulp.task('vendor_fonts', function () {
   gulp.src(paths.src.vendor_fonts_css)
     .pipe(concat('fonts.css'))
     .pipe(gulp.dest(paths.dest.vendor_fonts));
   return gulp.src(paths.src.vendor_fonts_font)
     .pipe(gulp.dest(paths.dest.vendor_fonts));
 });
+
+gulp.task('clean', function () {
+  del('./dist');
+});
+
+gulp.task('build', ['clean', 'scripts', 'less', 'images', 'vendor_scripts', 'vendor_styles', 'vendor_styles', 'vendor_fonts']);
 
 gulp.task('default', ['scripts', 'less', 'images', 'vendor_scripts', 'vendor_styles', 'vendor_styles', 'vendor_fonts']);
 
